3ff286a9f43521d823e341d4d30c80ff98bb3799
argshook <argshook@gmail.com>
2016-08-04 00:07:43 +0300

    reduce move dependencies to be handled in bundle.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* globals HTMLImports, Polymer */
 
+import '../node_modules/webcomponents.js/webcomponents-lite.min.js';
+
 import helloWorld from './components/hello-world/hello-world.js';
+import polymerImport from './components/polymer-import/polymer-import.js';
 
 HTMLImports.whenReady(() => {
   Polymer(helloWorld);
