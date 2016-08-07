8812ee5e60839e7c5382ad66efaaec3f2b70a6e2
argshook <argshook@gmail.com>
2016-08-08 01:02:38 +0300

    turbify angular example
    events go in, events come out

--- src/components/pr-clock/pr-clock.js
+++ src/components/pr-clock/pr-clock.js
@@ -8,17 +8,50 @@ function prClock() {
         type: String,
         value: getTime()
       },
+      format: {
+        type: String,
+        value: '24h',
+        observer: '_formatChanged',
+        notify: true
+      }
     },
     ready() {
       setInterval(() => {
         this.time = getTime();
       }, 1000);
     },
-    getTimeString: t => `${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}`
+    getTimeString(t, format) {
+      switch (format) {
+        default:
+        case '24h':
+          return `${pad(t.getHours())}:${pad(t.getMinutes())}:${pad(t.getSeconds())}`;
+
+        case '12h':
+          return `${pad(hours12(t.getHours()))}:${pad(t.getMinutes())}:${pad(t.getSeconds())} ${amOrPm(t.getHours())}`;
+      }
+    },
+    _formatChanged(newValue) {
+      console.log('pr-clock: time format changed to', newValue);
+    },
+    handleClick() {
+      this.fire('custom-polymer-event', 'hello!');
+    }
   };
 }
 
 function getTime() {
   return new Date();
+}
+
+function hours12(hours) {
+  return hours > 12 ? hours - 12 : hours === 0 ? 12 : hours;
+}
+
+function amOrPm(hours) {
+  return hours > 12 ? 'PM' : 'AM';
+}
+
+function pad(n) {
+  return n < 10 ? '0' + n : n;
 }
 
