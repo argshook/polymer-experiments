a5a83fa5dcdc43334b2ce605675ae526430e9d57
argshook <argshook@gmail.com>
2016-08-09 15:43:50 +0300

    refactor to use webcomponentsjs and polymer from bower

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 /* globals Polymer */
 
-import '../node_modules/webcomponents.js/webcomponents.min.js';
-
-import './polymer.js';
 import helloWorld from './components/hello-world/hello-world.js';
 import prClock from './components/pr-clock/pr-clock.js';
 
